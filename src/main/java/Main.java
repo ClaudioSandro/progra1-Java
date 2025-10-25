d2e76132dde93596b2fb844d4fc83f7b149cbf58
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2025-10-24 19:45:44 -0500

    chore: extract "Hello world" logic into a separate method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+
+    // Print Hello World!
+    public static void helloWorld(){
         System.out.println("Hello world!");
+    }
+
+
+    public static void main(String[] args) {
     }
 }
