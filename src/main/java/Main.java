f6d017adbbad646263da6a99559afb3b0fade248
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2025-10-24 19:46:34 -0500

    chore: add `beerWithUs` method to interactively determine drinking eligibility

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     // Print Hello World!
@@ -5,7 +7,28 @@ public class Main {
         System.out.println("Hello world!");
     }
 
+    public static void beerWithUs(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Write your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Write your age: ");
+        int age = sc.nextInt();
+
+
+        System.out.println("Hello " + name + ", your age is " + age);
+
+        if (age >= 18){
+            System.out.println("You can drink with us!");
+        } else{
+            System.out.println("You can't drink with us!, you are too young");
+        }
+    }
+
 
     public static void main(String[] args) {
+        helloWorld();
+        beerWithUs();
     }
 }
